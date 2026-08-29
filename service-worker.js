b2e2317230cc78f2274cbc3e4eb93060563f1d78
schwartzes44-code <schwartz.es44@gmail.com>
2026-08-29 14:13:02 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,21 @@
-const CACHE_NAME = 'je-microcredito-pwa-v1';
+const CACHE_NAME = 'je-microcredito-pwa-v2';
 
 const APP_ASSETS = [
   './',
   './index.html',
-  './manifest.webmanifest'
+  './manifest.webmanifest',
+  './icon.svg'
 ];
 
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_ASSETS)));
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
-      )
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
     )
   );
   self.clients.claim();
@@ -28,19 +23,13 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   if (event.request.method !== 'GET') return;
-
   event.respondWith(
     fetch(event.request)
       .then(response => {
         const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, copy);
-        });
+        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
         return response;
       })
-      .catch(() =>
-        caches.match(event.request)
-          .then(cached => cached || caches.match('./index.html'))
-      )
+      .catch(() => caches.match(event.request).then(cached => cached || caches.match('./index.html')))
   );
 });
